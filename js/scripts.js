4297fcae6247cd73e07591070d54e5d0966b1cab
Danyel Murray <danyelmurray1987@gmail.com>
2017-12-15 11:00:00 -0800

    adding input fields, checkboxes and buttons to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+Pizza.prototype.displayPrice = function(){
+return "Your pizza costs $" + (this.price);
+}
+
 function Pizza(size, price, sauce, cheese, meat, veggies) {
   this.size = size;
   this.price = 0;
@@ -9,27 +13,21 @@ function Pizza(size, price, sauce, cheese, meat, veggies) {
     this.price += 15;
   }else if (this.size === "medium") {
     this.price += 20;
-  }else if (this.size === "large") {
+  }else if(this.size === "large") {
     this.price += 25;
   }
-
   if (this.meat === "pepperoni", "sausage", "bacon") {
     this.price += 2;
   }else if (this.meat === "none"){
     this.price = 0;
   }
 
-  if (this.cheese === "none") {
-    this.price -= 2;
-  }
-
   this.price = this.price
 
   }
 
-  Pizza.prototype.displayPrice = function(){
-  return "Your pizza costs $" + Math.round(this.price);
-  }
+
+
 
 
   $(document).ready(function(){
@@ -41,8 +39,6 @@ function Pizza(size, price, sauce, cheese, meat, veggies) {
 //    var name = $("#movie[name]").val();
     var size = $("input:radio[name=size]:checked").val();
     var meat = $("input:radio[name=meat]:checked").val();
-    var cheese = $("input:radio[name=cheese]:checked").val();
-    var veggies = $("input:radio[name=veggies]:checked").val();
 
     // var student = $("input:radio[name=student]:checked").val();
     // var time = $("input:radio[name=time]:checked").val();
