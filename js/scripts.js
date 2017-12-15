81c2244f13becd5c010ff983fa0cd8d91e415b73
Danyel Murray <danyelmurray1987@gmail.com>
2017-12-15 10:12:23 -0800

    get math functions working with if and else statements, add buttons and topping selections.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,30 @@
 function Pizza(size, price, sauce, cheese, meat, veggies) {
   this.size = size;
-  this.price = price;
+  this.price = 0;
   this.sause = sauce;
   this.cheese = cheese;
   this.meat = meat;
   this.veggies = veggies;
-  if (this.size = "small"){
+  if (this.size === "small") {
     this.price += 15;
-  }else if (this.size = "medium") {
+  }else if (this.size === "medium") {
     this.price += 20;
-  }else if (this.price = "large") {
+  }else if (this.size === "large") {
     this.price += 25;
   }
-  this.price = this.price + this.price
-  console.log(this.price)
+
+  if (this.meat === "pepperoni", "sausage", "bacon") {
+    this.price += 2;
+  }else if (this.meat === "none"){
+    this.price = 0;
+  }
+
+  if (this.cheese === "none") {
+    this.price -= 2;
+  }
+
+  this.price = this.price
+
   }
 
   Pizza.prototype.displayPrice = function(){
@@ -23,15 +34,20 @@ function Pizza(size, price, sauce, cheese, meat, veggies) {
 
   $(document).ready(function(){
   $("form#orderForm").submit(function(event){
+    debugger;
     event.preventDefault();
     $("#result").empty();
-    var pizza = $("#pizza").val();
+    //var pizza = $("#pizza").val();
 //    var name = $("#movie[name]").val();
     var size = $("input:radio[name=size]:checked").val();
+    var meat = $("input:radio[name=meat]:checked").val();
+    var cheese = $("input:radio[name=cheese]:checked").val();
+    var veggies = $("input:radio[name=veggies]:checked").val();
+
     // var student = $("input:radio[name=student]:checked").val();
     // var time = $("input:radio[name=time]:checked").val();
     // var number = $("input.number").val();
-     var newTickets = new Pizza(size)//, price, sauce, cheese, meat, veggies);
+     var newPizza = new Pizza(size)//, price, sauce, cheese, meat, veggies);
     $("#result").append(newPizza.displayPrice());
     //$("#result").append(newTickets.displayPoster());
   });
