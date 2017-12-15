1525a57de921c5de1327911e3c39296169856d70
Danyel Murray <danyelmurray1987@gmail.com>
2017-12-15 13:51:30 -0800

    add result display containing name and ccost

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+function Contact(name, phone, address) {
+  this.name = name;
+  this.phone = phone;
+  this.address = address;
+}
+
+
 function Pizza(size, price, sauce, cheese, meat, veggies) {
   this.size = size;
   this.price = 0;
@@ -11,47 +18,43 @@ function Pizza(size, price, sauce, cheese, meat, veggies) {
       this.price += 20;
     }else if(this.size === "large") {
       this.price += 25;
-  }
-  // var veggies = this.veggies.toString();
-  // for(var i = 0; i < veggies.length; i++) {
-  //   this.price += 1;
-  // }
-  if (this.cheese === "none") {
-    this.price -= 1;
-  }
-}
-  Pizza.prototype.displayPrice = function(){
-  return "Your pizza costs $" + (this.price);
-}
+      }
+   //var veggies = this.veggies.push();
+    // for(var i = 0; i < veggies.length; i++) {
+    // this.price += 1;
+    // }
 
-function contactInfo(name, phone, address) {
-  this.name = name;
-  this.phone = phone;
-  this.address = address;
+    // if (this.cheese === "none") {
+    // this.price -= 1;
+    // }
 
-
-  contactInfo.prototype.display = function() {
-  return (this.name) + (this.phone) + (this.address);
-  }
 }
+    Pizza.prototype.displayPrice = function(){
+    return "Your pizza costs $" + (this.price) + ". ";
+    }
+
+    Contact.prototype.fullName = function() {
+      return "Thank you, " + (this.name)
+    }
+
 
 
   $(document).ready(function(){
   $("form#orderForm").submit(function(event){
-    //debugger;
+    debugger;
     event.preventDefault();
     $("#result").empty();
-    //var pizza = $("#pizza").val();
-//    var name = $("#movie[name]").val();
     var size = $("input:radio[name=size]:checked").val();
     var meat = $("input:checkbox[name=meat]:checked").val();
     var cheese = $("input:radio[name=cheese]:checked").val();
     var veggies = $("input:checkbox[name=veggies]:checked").val();
     var name = $("input#name").val();
+    var phone = $("input#phone").val();
+    var address = $("input#address").val();
     var newPizza = new Pizza(size, cheese);
-     //var newContact = new ContactInfo(name, phone, address);
+    var newContact = new Contact(name, phone, address);
     $("#result").append(newPizza.displayPrice());
-    //$("#result").append(newContact.contact());
+    $("#result").append(newContact.fullName());
     //$("#result").append(newTickets.displayPoster());
   });
 });
