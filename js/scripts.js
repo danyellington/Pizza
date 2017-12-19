a801055bbd177a2fd6b5c1f2971bdd63e849e426
Danyel <danyelmurray1987@gmail.com>
2017-12-18 18:47:55 -0800

    worked out bugs

--- js/scripts.js
+++ js/scripts.js
@@ -7,33 +7,71 @@ Contact.prototype.fullName = function() {
   return "Thank you, " + (this.name) + ". ";
 }
 
-function Pizza(size, price, sauce, cheese, meat, veggies) {
+function Pizza(size, cheese, toppings) {
   this.size = size;
   this.cheese = cheese;
-  this.meat = [];
-  this.veggies = [];
+  this.topping = toppings;
   //this.number = number;
   this.price = 0;
-  if (this.cheese === "none") {
-    this.price -=2;
-  }
-  if (this.size === "small") {
-    this.price += 15;
-    }else if (this.size === "medium") {
-      this.price += 20;
-    }else if(this.size === "large") {
-      this.price += 25;
-    }
+  // toppings.forEach(function(thing){
+  //   this.topping.push(thing);
+  // })
 }
+
 
 //   function veggieCost (veggies) {
 //   for(var i = 0; i<= veggies.length; i++){
 // }
 // }
-Pizza.prototype.displayPrice = function(){
-  return "Your pizza costs $" + (this.price) + ".";
+Pizza.prototype.calcPrice = function(){
+  if (this.cheese === "none") {
+    this.price -=2;
+  } else if (this.cheese === "extra") {
+    this.price += 2;
+  }
+
+  if (this.topping.includes("pepperoni")) {
+    this.price += 2;
+  }
+  if (this.topping.includes("sausage")) {
+    this.price += 2.5;
+  }
+  if (this.topping.includes("bacon")) {
+      this.price += 2;
+  }
+  if (this.topping.includes("chicken")) {
+      this.price += 3;
+  }
+  if (this.topping.includes("onion")) {
+    this.price += 1.5;
+  }
+  if (this.topping.includes("peppers")) {
+    this.price += 2;
+  }
+  if (this.topping.includes("olives")) {
+    this.price += 1.75;
+  }
+  if (this.topping.includes("mushroom")) {
+    this.price += 2;
+  }
+  console.log(this.price);
+  if (this.size === "small") {
+    this.price += 15;
+    } else if (this.size === "medium") {
+      this.price += 20;
+    } else if(this.size === "large") {
+      this.price += 25;
+    }
+
 }
 
+Pizza.prototype.displayPrice = function(){
+  return " Your pizza costs $" + (this.price) + ".";
+}
+
+Contact.prototype.displayContacts = function(){
+  return " Your pizza will be delivered to " + this.address + "." + " We can contact you at " + this.phone + "."
+}
 
 //
 //     var meat = this.meat function(); {
@@ -45,22 +83,29 @@ Pizza.prototype.displayPrice = function(){
 //UI
   $(document).ready(function(){
   $("form#orderForm").submit(function(event){
-      debugger;
+      // debugger;
       event.preventDefault();
       $("#result").empty();
+        var toppings = [];
         var size = $("input:radio[name=size]:checked").val();
-        var meat = $("input:checkbox[name=meat]:checked").val();
+        var meat = $("input:checkbox[name=meat]:checked").each(function() {
+          toppings.push($(this).val())
+        });
         var cheese = $("input:radio[name=cheese]:checked").val();
-        var veggies = $("input:checkbox[name=veggies]:checked").val();
+        var veggies = $("input:checkbox[name=veggies]:checked").each(function() {
+          toppings.push($(this).val())
+        });
         var name = $("input#name").val();
         var phone = $("input#phone").val();
         var address = $("input#address").val();
         var number = $("input.number").val();
-        var newPizza = new Pizza(size, cheese);
+        var newPizza = new Pizza(size, cheese, toppings);
+        console.log(newPizza);
         var newContact = new Contact(name, phone, address);
       $("#result").append(newContact.fullName());
+      newPizza.calcPrice();
       $("#result").append(newPizza.displayPrice());
-
+      $("#result").append(newContact.displayContacts());
 
   });
 });
